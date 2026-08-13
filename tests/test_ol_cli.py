3e946a990a9f5951971955f854512448315c7632
renanzai <renanzai@EVA-01.localdomain>
2026-08-14 02:02:03 +0800

    fix(test): restore OPENCODE_GO dummy keys + correct version asserts (0.7.0)

--- tests/test_ol_cli.py
+++ tests/test_ol_cli.py
@@ -18,7 +18,7 @@ class TestCLICommandLoading:
         assert app is not None
 
     def test_version_constant(self):
-        assert __version__ == "0.6.0"
+        assert __version__ == "0.7.0"
 
 
 class TestVersionFlag:
